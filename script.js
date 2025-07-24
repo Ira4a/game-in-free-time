7bd1037d9334672c02de0523c2442493caaf342e
Irada A. <clan.i4a@gmail.com>
2025-07-24 14:43:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const myGameArea = {
   canvas: document.createElement("canvas"),
   start: function() {
     this.canvas.width = 800;
-    this.canvas.height = 500;
+    this.canvas.height = 400;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.firstChild);
     this.frameNo = 0;
